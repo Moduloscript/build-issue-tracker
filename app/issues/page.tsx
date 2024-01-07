f22ff1013e9fba3a9d61b6349455467e7b84e26e
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-01-07 06:59:42 +0100

    set Up Radix Ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Flex, Text, Button } from '@radix-ui/themes';
+
+
+
 
 const IssuesPage = () => {
   return (
-    <div>
-      issues Page
-    </div>
+    <Button>Let's go</Button>
   )
 }
 
