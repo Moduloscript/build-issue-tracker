939ab9e9e2045df69b221e77ef241092e807f512
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-01-09 23:08:24 +0100

    added the Loading Skeleton component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,16 +3,16 @@ import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
+import delay from "delay"
+import IssueAction from "./IssueAction";
+
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
+  await delay(2000)
   return (
     <>
-      <div className="mb-5">
-        <Button>
-          <Link href="/issues/new">New Issue 💻</Link>
-        </Button>
-      </div>
+      <IssueAction/>
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
