51364829ce396ebd72b19408fae0536030383630
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-01-11 14:31:41 +0100

    added Mark preview functionality

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,7 +2,7 @@ import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import React from "react";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   params: { id: string };
@@ -21,8 +21,8 @@ const IssueDetailPage = async ({ params }: Props) => {
         <p>{issue.status}</p>
         <p>{issue.createAt.toDateString()}</p>
       </Flex>
-      <Card>
-        <Text>{issue.description}</Text>
+      <Card className="prose  mt-4" >
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   );
