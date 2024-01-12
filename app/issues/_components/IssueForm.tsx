fc838e7abc5b7aaa633f780dd1def3704c5a78f6
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-01-12 13:14:55 +0100

    final update for validation schema 😊😊😊

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -4,7 +4,7 @@ import {
     ErrorMessage,
     Spinner,
 } from "@/app/components";
-import { createIssueSchema } from "@/app/validationSchema";
+import { issueSchema } from "@/app/validationSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Issue } from "@prisma/client";
 import { Button, Callout, TextField } from "@radix-ui/themes";
@@ -20,7 +20,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 
 const IssueForm = ({issue}: {issue? : Issue}) => {
@@ -31,7 +31,7 @@ const IssueForm = ({issue}: {issue? : Issue}) => {
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
   const [error, setError] = useState("");
   const [submitForm, setSubmitForm] = useState(false);
