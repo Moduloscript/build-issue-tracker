851e39cce184ddd40df1c5e472a0b77a63fc37cf
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-01-14 14:50:16 +0100

    implemented a Dialog confirmation Button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,34 @@
-import { Button } from "@radix-ui/themes"
+"use client";
 
-const DeleteIssueButton = ({issueId}: {issueId: number}) => {
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      Delete Button
-    </Button>
-  )
-}
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Button</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are You Sure You want to delete this issue? This Action cannot be
+            Undone.
+          </AlertDialog.Description>
+          <Flex mt="4" gap="3">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel the Deletion
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red">Delete Issue</Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
+  );
+};
 
-export default DeleteIssueButton
+export default DeleteIssueButton;
