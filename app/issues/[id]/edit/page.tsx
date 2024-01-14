57587186b8721e333d3a4307ebf9dfd8e819fe71
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-01-14 14:06:52 +0100

    improve the loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import IssueForm from "../../_components/IssueForm";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"),
+{
+  ssr: false,
+  loading: () => <IssueFormSkeleton />
+});
 interface Props {
   params: { id: string };
 }
@@ -11,7 +18,7 @@ const EditIssuePage = async ({ params }: Props) => {
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
-  if(!issue) notFound()
+  if (!issue) notFound();
   return <IssueForm issue={issue} />;
 };
 
