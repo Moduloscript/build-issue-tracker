3e2d5a946a03447dd8d95af616c96dcd70dba5a1
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-01-18 13:30:01 +0100

    add the DropdownMenu Feature 💫

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,11 +5,11 @@ import { AiFillBug } from "react-icons/ai";
 import { usePathname } from "next/navigation";
 import classnames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes";
 
 const Navbar = () => {
   const pathNameDirective = usePathname();
-  const { status, data } = useSession();
+  const { status, data: session } = useSession();
 
   const links = [
     {
@@ -50,7 +50,26 @@ const Navbar = () => {
           </Flex>
           <Box>
             {status === "authenticated" && (
-              <Link href="/api/auth/signout">Logout</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback={""}
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                 <Link href="/api/auth/signout">Logout</Link>
+
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             )}
             {status === "unauthenticated" && (
               <Link href="/api/auth/signin">LogIn Please</Link>
