3777b4f9d691cf5b64a458dbb3e989092f968187
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-01-21 20:43:14 +0100

    build the layout of the pagination Component 📃🎡

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import Pagination from "./components/Pagination";
 
 
 export default function Home() {
   return (
-  <h1>Hello world </h1>
+  <Pagination itemCount={100} pageSize={10} currentPage={2}/>
   )
 }
