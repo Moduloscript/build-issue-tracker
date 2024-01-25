ef09a3f4f49b1a430d52cdd1a6ac25c123980a9d
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-01-26 00:47:52 +0100

    set ip error Tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://edb7d75b9ab238af8a1e6835bf227b68@o4506634538450944.ingest.sentry.io/4506634541727744",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
